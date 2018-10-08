1acaa32951b065c95183690bf19df8483f1eef55
Lake Hu <leonshark@hotmail.com>
2018-10-08 11:16:13 +0800

    find device by ip and id is asset. update device by serial id and states. device factory will create devices through KV files.

--- Device.cpp
+++ Device.cpp
@@ -1,9 +1,23 @@
 #include "Device.h"
 
-Device::Device(char id[]):id_(id) {
+Device::Device(const char ip[], const char id[]):ip_(ip), id_(id) {
 
 }
 
-void Device::update() {
+void Device::update(int sid, int addr, const std::vector<char> &stats) {
 
+}
+
+///////////////////////////////////////////////////////////////////////
+DeviceFactory::DeviceFactory() {
+  keyFile_ = g_key_file_new();
+}
+
+DeviceFactory::~DeviceFactory() {
+  g_key_file_free(keyFile_);
+}
+
+std::vector<Device*> createDevices() {
+  std::vector<Device*> devs;
+  return devs;
 }
--- Device.h
+++ Device.h
@@ -2,13 +2,32 @@
 #define _DEVICE_H_
 
 #include <string>
+#include <vector>
+#include <glib.h>
 
 class Device {
  private:
+  const std::string ip_;
   const std::string id_;
  public:
-  Device(char id[]);
-  void update();
+  Device(const char ip[], const char id[]);
+  Device* equalIP(const std::string &ip) const {
+    return ip_ == ip ? true :false;
+  }
+  Device* equalID(const std::string &id) const {
+    return id_ == id ? true : false;
+  }
+  void update(int sid, int addr, const std::vector<char> &stats);
+};
+
+class DeviceFactory {
+ private:
+  GKeyFile *keyFile_;
+ public:
+  DeviceFactory();
+  ~DeviceFactory();
+
+  std::vector<Device*> createDevices();
 };
 
 #endif
