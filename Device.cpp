e9a37801a2b3d489d92d0d22520f65686ef2a54b
Lake Hu <leonshark@hotmail.com>
2018-10-19 14:12:41 +0800

    create device and operations. execute it when state was changed.

--- Device.cpp
+++ Device.cpp
@@ -7,7 +7,16 @@ Device::Device(const char ip[], const char id[]):ip_(ip), id_(id) {
 }
 
 void Device::update(int sid, const std::vector<char> &stats) {
-
+  int idx = 0;
+  for(char s : stats) {
+    for(Operation *oper : opers_) {
+      if(oper->equalPort(sid) && oper->equalAddr(idx)) {
+        oper->execute(s);
+        break; //TODO one state for one operation
+      }
+    }
+    idx += 1;
+  }
 }
 
 void Device::clearOpers() {
@@ -32,6 +41,7 @@ std::vector<Device*> DeviceFactory::createDevices() {
     syslog(LOG_CRIT, "Device configure failed! (%s)", error->message);
     return devs; //TODO sth will do
   }
+  OperationDefine opdef;
   gchar **groups = g_key_file_get_groups(keyFile_, NULL);
   while(groups != NULL) {
     gchar* id = g_key_file_get_string(keyFile_, *groups, "id", &error);
@@ -39,6 +49,13 @@ std::vector<Device*> DeviceFactory::createDevices() {
     Device *dev = new Device(ip, id);
     devs.push_back(dev);
     //TODO create operators of device.
+    char opfile[64];
+    g_snprintf(opfile, 64, "op_%s.ini", (*groups));
+    syslog(LOG_INFO, "Current Operation Conf : %s", opfile);
+    std::vector<Operation*> ops = opdef.create(opfile);
+    dev->setOpers(ops);
+    groups += 1;
   }
   return devs;
 }
+
