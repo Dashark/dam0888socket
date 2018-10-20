d24eddecc4b1bf95abaf1759ee5d5540304b96df
Lake Hu <leonshark@hotmail.com>
2018-10-20 11:21:47 +0800

    fix bug

--- Device.cpp
+++ Device.cpp
@@ -43,7 +43,7 @@ std::vector<Device*> DeviceFactory::createDevices() {
   }
   OperationDefine opdef;
   gchar **groups = g_key_file_get_groups(keyFile_, NULL);
-  while(groups != NULL) {
+  while(*groups != NULL) {
     gchar* id = g_key_file_get_string(keyFile_, *groups, "id", &error);
     gchar* ip = g_key_file_get_string(keyFile_, *groups, "ip", &error);
     Device *dev = new Device(ip, id);
