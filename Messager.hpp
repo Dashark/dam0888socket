077fba78cdc1c7958813387b7ec60cbe6b224fa4
minwei <minweiilovyou@gmail.com>
2019-01-05 09:04:22 +0800

    增加SmartMeterMessager类

--- Messager.hpp
+++ Messager.hpp
@@ -35,7 +35,7 @@ public:
   virtual bool send(Broker *bro);
   virtual void dump();
 };
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class DeviceStatusMessager : public Messager {
 private:
   json js_;
@@ -50,7 +50,23 @@ public:
   virtual bool send(Broker *bro);
   virtual void dump();
 };
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class SmartMeterMessager : public Messager {
+private:
+  json js_;
+public:
+  SmartMeterMessager();
+  virtual ~SmartMeterMessager();
+  virtual void setID(const std::string &id);
+  virtual bool setAStep(const std::string &step);
+  virtual bool setTime(const std::string &time);
+  virtual bool setKV(const std::string &key, const std::string &val);
+  virtual bool setDID(const std::string &did);
+  virtual bool send(Broker *bro);
+  virtual void dump();
+};
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class MessagerDefine {
 public:
   static Messager* create(const std::string &name);
